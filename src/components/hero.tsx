ef713115780db431c5a5a5ed3c445d42c5f126e8
John Bakhmat <johnbakhmat@gmail.com>
2024-12-05 17:51:19 +0300

    feat: add hi im john

---
+++ src/components/hero.tsx
@@ -0,0 +1,11 @@
+export const Hero = () => {
+	return (
+		<div className="grid place-items-center grid-rows-[1ft_auto_1fr]">
+			<div />
+			<h1 className="text-7xl">
+				Hi, I'm <span className="text-john">John</span>
+			</h1>
+			<h2>I build software</h2>
+		</div>
+	);
+};
