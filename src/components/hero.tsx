b0f28a43f82046ed4cf279b3d31bf6f3d472e60a
John Bakhmat <johnbakhmat@gmail.com>
2024-12-22 19:37:36 +0300

    fix: some mobile / tablet fixes

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,11 +1,11 @@
 export const Hero = () => {
   return (
-    <div className="grid place-items-center grid-rows-[1ft_auto_1fr] px-32 py-20 rounded-2xl">
+    <div className="grid place-items-center grid-rows-[1ft_auto_1fr] lg:px-32 lg:py-20 rounded-2xl">
       <div />
-      <h1 className="text-7xl">
+      <h1 className="text-5xl md:text-6xl lg:text-7xl">
         Hi, I'm <span className="text-john">John</span>
       </h1>
-      <h2 className="text-3xl">I build software</h2>
+      <h2 className="text-xl md:text-2xl lg:text-3xl">I build software</h2>
     </div>
   );
 };
