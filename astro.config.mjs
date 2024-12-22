889ace008628558f3e4130414ab2a61c2080f2bb
John Bakhmat <johnbakhmat@gmail.com>
2024-12-22 19:19:26 +0300

    feat: deno adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
-import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+import deno from "@deno/astro-adapter";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), mdx(), sitemap()],
-
-  adapter: node({
-    mode: "standalone",
-  }),
+	integrations: [react(), tailwind(), mdx(), sitemap()],
+	adapter: deno(),
 });
