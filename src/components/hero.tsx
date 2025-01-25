c46c0780b8ed2de64558b1632232f39bf44101ed
John Bakhmat <johnbakhmat@gmail.com>
2025-01-25 10:37:41 +0300

    feat: hero blur

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,6 +1,6 @@
 export const Hero = () => {
   return (
-    <div className="grid place-items-center grid-rows-[1ft_auto_1fr] lg:px-32 lg:py-20 rounded-2xl">
+    <div className="grid place-items-center grid-rows-[1ft_auto_1fr] lg:px-32 lg:py-20 rounded-3xl bg-transparent backdrop-blur-md">
       <div />
       <h1 className="text-5xl md:text-6xl lg:text-7xl">
         Hi, I'm <span className="text-john">John</span>
